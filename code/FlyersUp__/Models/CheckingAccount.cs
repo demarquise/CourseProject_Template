6f2d27ceeef72a1c545c8dd17f7647f30a1c497e
demarquise boone <demarquise_boone@yahoo.com>
2018-12-06 11:35:31 -0500

    UI i worked on

--- code/FlyersUp__/Models/CheckingAccount.cs
+++ code/FlyersUp__/Models/CheckingAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,9 +9,28 @@ namespace FlyersUp__.Models
     public class CheckingAccount
     {
         public int Id { get; set; }
+
+        [Required]
+        [RegularExpression(@"\d{6,10}",ErrorMessage ="Account must be between 6 - 10 characters")]
+        [Display(Name="Account #")]
         public string AccountNumber { get; set; }
+
+        [Required]
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
+
+        public string Name
+        {
+            get
+            {
+                return string.Format("{0} {1}", this.FirstName, this.LastName);
+            }
+        }
+        [DataType(DataType.Currency)]
         public decimal Balance { get; set; }
 
     }
